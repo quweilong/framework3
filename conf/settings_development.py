77b03ee968824b427aa3e9ed12bf544907f36a03
quweilong <qwl120339@163.com>
2019-12-06 20:07:11 +0800

    22222222wwwwwww

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -19,7 +19,7 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'test1',                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'mysql',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                        # 你的数据库user
         'PASSWORD': 'L*vIW=.lTr',                        # 你的数据库password
         'HOST': '192.168.0.182',                   # 开发的时候，使用localhost
