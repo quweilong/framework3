c5e5b168d762b1af9cde37d11b9f95ce09314e97
quweilong <qwl120339@163.com>
2019-12-06 20:04:31 +0800

    22222222wwwwwww

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -21,8 +21,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'test1',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                        # 你的数据库user
-        'PASSWORD': '120339',                        # 你的数据库password
-        'HOST': '10.10.0.92',                   # 开发的时候，使用localhost
+        'PASSWORD': 'L*vIW=.lTr',                        # 你的数据库password
+        'HOST': '192.168.0.182',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -21,8 +21,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'test1',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': '120339',                        # 你的数据库password
-        'HOST': '10.10.0.92',                   		   # 数据库HOST
+        'PASSWORD': 'L*vIW=.lTr',                        # 你的数据库password
+        'HOST': '192.168.0.182',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
